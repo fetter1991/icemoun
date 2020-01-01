7ca1e29f7f52c6f2b7498d9e17872ee5f560242e
610481663@qq,com <Pei5098252>
2020-01-02 01:31:48 +0800

    采集预览

--- App/Back/Controller/SeekMoviesController.class.php
+++ App/Back/Controller/SeekMoviesController.class.php
@@ -11,51 +11,100 @@ namespace Back\Controller;
 
 use QL\QueryList;
 
-class SeekMoviesController extends CommonController {
-	private $table = 'ice_comic_copy';
-	private $server = 'https://www.18comic.life/';
+class SeekMoviesController extends CommonController
+{
+    private $table = 'ice_comic';
 
-	public function __construct() {
-		parent::__construct();
-		import( 'Common.Lib.Page' );
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        import('Common.Lib.Page');
+    }
 
-	/**
-	 * 求片列表
-	 */
-	public function index() {
-		$count = M( $this->table )->where( 'status = 1' )->count( '1' );
-		$page  = new \Common\Page( $count, 15 );
-		$list  = M( $this->table )->where( 'status = 1' )
-		                          ->limit( $page->firstRow, $page->listRows )
-		                          ->order( 'id desc' )
-		                          ->select();
-		foreach ( $list as $key => $item ) {
-			$path                    = iconv( "utf-8", "gbk", $item['name'] );
-			$res                     = file_exists( __BOOKS__ . $path );
-			$list[ $key ]['isExist'] = $res ? 1 : 0;
-		}
+    /**
+     * 求片列表
+     */
+    public function index()
+    {
+        $keyword = I('keyword');
+        if ($keyword && is_numeric($keyword)) {
+            $map['link'] = array('like', '%' . $keyword . '%');
+            $map['db_id'] = array('like', '%' . $keyword . '%');
+            $map['_logic'] = 'or';
+            $where['_complex'] = $map;
+        } elseif ($keyword && !is_numeric($keyword)) {
+            $map['name'] = array('like', '%' . $keyword . '%');
+            $map['_logic'] = 'or';
+            $where['_complex'] = $map;
+        }
+        $where['db_id'] = array('neq', 0);
 
-		$this->assign( 'list', $list );
-		$this->assign( 'page', $page->show() );
-		$this->display();
-	}
+        $count = M($this->table)->where($where)->count('1');
+        $page = new \Common\Page($count, 15);
+        $list = M($this->table)->where($where)
+            ->limit($page->firstRow, $page->listRows)
+            ->order(array('status' => 'asc'))
+            ->select();
+        foreach ($list as $key => $item) {
+            $path = iconv("utf-8", "gbk", $item['name']);
+            $res = file_exists(__BOOKS__ . $path);
+            if ($res) {
+                $list[$key]['cover'] = 'http://127.0.0.1/Books/' . $item['name'] . '/00001.jpg';
+            } else {
+                $list[$key]['cover'] = '';
+            }
+        }
 
-	public function getInfo() {
-		$id   = I( 'id' );
-		$info = M( $this->table )->where( 'id = ' . $id )->find();
-		$str  = '';
-		for ( $i = 1; $i <= $info['total_page']; $i ++ ) {
-			$p   = str_pad( $i, 5, "0", STR_PAD_LEFT );
-			$str .= "https://cdn-ms.18comic.life/media/photos/" . $info['db_id'] . '/' . $p . ".jpg\n";
-		}
-		$info['str'] = $str;
+        $this->assign('list', $list);
+        $this->assign('page', $page->show());
+        $this->display();
+    }
 
-		if ( $info ) {
-			$this->ajaxReturn( array( 'code' => 200, 'data' => $info ) );
-		} else {
-			$this->ajaxReturn( array( 'code' => 0, 'data' => array() ) );
-		}
-	}
+    public function getInfo()
+    {
+        $id = I('id');
+        $info = M($this->table)->where('id = ' . $id)->find();
+        $str = '';
+        for ($i = 1; $i <= $info['total_page']; $i++) {
+            $p = str_pad($i, 5, "0", STR_PAD_LEFT);
+            $str .= "https://cdn-ms.18comic.life/media/photos/" . $info['db_id'] . '/' . $p . ".jpg\n";
+        }
+        $info['str'] = $str;
+
+        if ($info) {
+            $this->ajaxReturn(array('code' => 200, 'data' => $info));
+        } else {
+            $this->ajaxReturn(array('code' => 0, 'data' => array()));
+        }
+    }
+
+    public function doEdit()
+    {
+        $data = I('post.');
+
+        if (!$data['id']) {
+            $this->ajaxReturn(array('code' => 0, 'msg' => 'ID不能为0'));
+        }
+        $saveData['name'] = trim($data['name']);
+        $saveData['org_name'] = trim($data['org_name']);
+        $saveData['tags'] = trim($data['tags']);
+        $saveData['total_page'] = intval($data['total_page']);
+        $saveData['desc'] = trim($data['desc']);
+        $saveData['add_time'] = time();
+
+        $path = iconv("utf-8", "gbk", $saveData['org_name']);
+        $isExist = file_exists(__BOOKS__ . $path);
+
+        if ($isExist) {
+            $saveData['status'] = 3;
+        }
+
+        $res = M($this->table)->where('id = ' . $data['id'])->save($saveData);
+        if ($res) {
+            $this->ajaxReturn(array('code' => 200, 'msg' => '保存成功'));
+        } else {
+            $this->ajaxReturn(array('code' => 0, 'msg' => '修改失败'));
+        }
+    }
 
 }
